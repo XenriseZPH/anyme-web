c68ea65d4b997e41c64243576c02a18616fa542b
XenDev PH <adagunan11@gmail.com>
2023-04-26 21:21:03 +0800

    🧪 test login using axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,29 @@
 import type { NextPage } from 'next';
 import { useAlert } from 'react-alert';
 import Head from 'next/head';
+import axios from '@/app/api';
 
 const Home: NextPage = () => {
   const alert = useAlert();
+
+  const handleLogin = async () => {
+    axios
+      .post('https://anilist.co/api/v2/oauth/token', {
+        grant_type: 'authorization_code',
+        client_id: '{12364}',
+        client_secret: '{pPS0DyhQbbWFHu5dknGTstwsUOItxN5lLMGpqPdn}',
+        redirect_uri: '{http://localhost:3000}', // http://example.com/callback
+        code: '{code}', // The Authorization Code received previously
+      })
+      .then((res) => {
+        console.log(res);
+        alert.success(res.data.message);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert.error(error?.response?.data?.message || error.message);
+      });
+  };
 
   return (
     <div className="font-inter">
@@ -18,9 +38,10 @@ const Home: NextPage = () => {
 
       <main>
         <div className="text-red-500 font-bold">Hello World</div>
-        <a href="https://anilist.co/api/v2/oauth/authorize?client_id={12364}&response_type=token">
+        <button onClick={handleLogin}>Login</button>
+        {/* <a href="https://anilist.co/api/v2/oauth/authorize?client_id={12364}&response_type=token">
           Login with AniList
-        </a>
+        </a> */}
       </main>
     </div>
   );
